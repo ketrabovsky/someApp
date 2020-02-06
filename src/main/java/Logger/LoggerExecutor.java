cc958d50ff68fe382f36ef426696acdce2bb7e6c
ktr <bertext@gmail.com>
2020-02-06 21:02:53 +0100

    Added conditions to LoggerExecutor

--- src/main/java/Logger/LoggerExecutor.java
+++ src/main/java/Logger/LoggerExecutor.java
@@ -1,16 +1,25 @@
 package Logger;
 
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class LoggerExecutor implements Runnable {
     private List<ILoggerHandler> handlers;
-    private Queue<String> msgQueue;
+    private BlockingQueue<String> msgQueue;
     private boolean running;
+    private Lock lock;
+    private Condition resourceArrivedCondition;
 
     public LoggerExecutor() {
         this.handlers = new ArrayList<>();
         this.msgQueue = new LinkedBlockingQueue<>();
+        this.lock = new ReentrantLock();
+        this.resourceArrivedCondition = lock.newCondition();
     }
 
     public void addHandler(ILoggerHandler handler) {
@@ -22,7 +31,13 @@ public class LoggerExecutor implements Runnable {
     }
 
     public void logMsg(String msg) {
-        msgQueue.add(msg);
+        lock.lock();
+        try {
+            msgQueue.add(msg);
+            resourceArrivedCondition.signal();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void start() {
@@ -36,11 +51,15 @@ public class LoggerExecutor implements Runnable {
     @Override
     public void run() {
         while (running) {
+            lock.lock();
             try {
+                this.resourceArrivedCondition.await(10, TimeUnit.MILLISECONDS);
                 String msg = msgQueue.remove();
                 passToHandlers(msg);
-            } catch (NoSuchElementException e) {
-                // Handle exception
+            } catch (NoSuchElementException | InterruptedException e) {
+                // nothing to do here
+            } finally {
+                lock.unlock();
             }
 
         }
